cd9ea617e55d11457c2d4a91e332c72fa2255dc9
Shivam Kumar <csautomaytic@gmail.com>
2025-03-14 17:49:26 +0530

    removed other 2 home pages.

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,32 @@
 import React, { Fragment } from 'react';
-import Header from '../components/header/Header';
-import Hero from '../components/hero/hero';
-import FeaturesSection from '../components/FeaturesSection/FeaturesSection';
-import About from '../components/about/about';
-import PolicySection from '../components/PolicySection/PolicySection';
-import ServiceSection from '../components/ServiceSection/ServiceSection';
-import ProjectSection from '../components/ProjectSection/ProjectSection';
-import Testimonial from '../components/Testimonial/Testimonial';
-import TeamSection from '../components/TeamSection/TeamSection';
-import BlogSection from '../components/BlogSection/BlogSection';
-// import CtaSection from '../components/CtaSection/CtaSection';
-import Footer from '../components/footer/Footer';
+import Header2 from '../components/Header2/Header2';
+import Hero2 from '../components/hero2/hero2';
+import FeaturePartners from '../components/software-company-components/FeaturePartners';
+import ServiceSection from '../components/software-company-components/ServiceSection/ServiceSection';
+import About from '../components/software-company-components/about/about';
+import ProcessTechnology from '../components/software-company-components/ProcessTechnology/ProcessTechnology';
+import FaqSection from '../components/software-company-components/FaqSection/FaqSection';
+import BlogSection from '../components/software-company-components/BlogSection/BlogSection';
+import ContactSection from '../components/software-company-components/ContactSection';
 import Scrollbar from '../components/scrollbar/scrollbar';
-
+import Footer from '../components/software-company-components/Footer/Footer';
+import CtaSection from '../components/CtaSection/CtaSection';
 
 const HomePage = () => {
-
     return (
         <Fragment>
             <div>
-                <Header />
+                <Header2 />
                 <main className="page_content">
-                    <Hero />
-                    <FeaturesSection />
-                    <About />
-                    <PolicySection />
+                    <Hero2 />
+                    <FeaturePartners />
                     <ServiceSection />
-                    <ProjectSection />
-                    <Testimonial />
-                    <TeamSection />
+                    <About />
+                    <ProcessTechnology />
+                    <FaqSection />
                     <BlogSection />
-                    {/* 
-                    <CtaSection /> */}
+                    <ContactSection />
+                    <CtaSection />
                 </main>
                 <Footer />
                 <Scrollbar />
